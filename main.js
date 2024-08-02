6764975caf2f1526fdb93adc460e2c84d190526a
Usman Ali <164320767+Usmanalicoder@users.noreply.github.com>
2024-08-02 15:00:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer", "Data Analytics", "Data Visualization"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
